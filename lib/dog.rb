f9437502d9afca663cedbfce331e2bda2b6d7d86
Karen Flores <karen.ag.flores@gmail.com>
2018-03-09 10:40:11 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name(dogname)
+    @name = dogname
+  end
+  def name = (dogname)
+    @name = dogname
+  end
+  def breed(dogbreed)
+    @breed = dogbreed
+  end
+  def breed = (dogbreed)
+    @breed = dogbreed
+  end
+end
